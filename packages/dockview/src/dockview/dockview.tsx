00dd9d920a9a701070278e0cdfc20b3f0a35829c
amano <armin.majerie@zero8.io>
2025-08-14 19:59:08 +0200

    fix the onCleanup issue

--- packages/dockview/src/dockview/dockview.tsx
+++ packages/dockview/src/dockview/dockview.tsx
@@ -74,90 +74,92 @@ function extractCoreOptions(props: IDockviewSolidProps): DockviewOptions {
 
 
 export function DockviewSolid(props: IDockviewSolidProps) {
-
-
   let domRef: HTMLDivElement | undefined;
   const [portals, addPortal] = usePortalsLifecycle();
   let prevProps: Partial<IDockviewSolidProps> = {};
   const [dockviewRef, setDockviewRef] = createSignal<DockviewApi | undefined>(undefined);
 
+  // Hold API so we can dispose it from the owner-registered cleanup
+  let api: DockviewApi | undefined;
 
   onMount(() => {
+    let disposed = false;
+
+    // ✅ Register cleanup synchronously (under owner)
+    onCleanup(() => {
+      disposed = true;
+      try { setDockviewRef(undefined); } catch {}
+      try { api?.dispose(); } catch {}
+      api = undefined;
+    });
+
+    // Defer creation to next frames, but DO NOT register onCleanup here
+    const start = () => {
+      if (!domRef || disposed) return;
+
+      const frameworkTabComponents = props.tabComponents ? { ...props.tabComponents } : {};
+      if (props.defaultTabComponent) {
+        frameworkTabComponents[DEFAULT_SOLID_TAB] = props.defaultTabComponent;
+      }
+
+      const frameworkOptions: DockviewFrameworkOptions = {
+        createLeftHeaderActionComponent: createGroupControlElement(
+          props.leftHeaderActionsComponent,
+          { addPortal }
+        ),
+        createRightHeaderActionComponent: createGroupControlElement(
+          props.rightHeaderActionsComponent,
+          { addPortal }
+        ),
+        createPrefixHeaderActionComponent: createGroupControlElement(
+          props.prefixHeaderActionsComponent,
+          { addPortal }
+        ),
+        createComponent: (options) => {
+          return new SolidPanelContentPart(
+            options.id,
+            props.components[options.name],
+            { addPortal }
+          );
+        },
+        createTabComponent: (options) => {
+          return new SolidPanelHeaderPart(
+            options.id,
+            (props.tabComponents ? props.tabComponents : {})[options.name] ??
+            (props.defaultTabComponent ? props.defaultTabComponent : undefined),
+            { addPortal }
+          );
+        },
+        createWatermarkComponent: props.watermarkComponent
+          ? () => new SolidWatermarkPart("watermark", props.watermarkComponent!, { addPortal })
+          : undefined,
+        defaultTabComponent: props.defaultTabComponent ? DEFAULT_SOLID_TAB : undefined,
+      };
+
+      api = createDockview(domRef, {
+        ...extractCoreOptions(props),
+        ...frameworkOptions,
+      });
+
+      const { clientWidth, clientHeight } = domRef;
+      api.layout(clientWidth, clientHeight);
+
+      props.onReady?.({ api });
+      setDockviewRef(api);
+    };
+
+    // Keep your 2× rAF delay, but only *call* start; don't register cleanups here
     requestAnimationFrame(() => {
       requestAnimationFrame(() => {
-        if (!domRef) {
-          return;
-        }
-
-        const frameworkTabComponents = props.tabComponents ? { ...props.tabComponents } : {};
-        if (props.defaultTabComponent) {
-          frameworkTabComponents[DEFAULT_SOLID_TAB] = props.defaultTabComponent;
-        }
-
-        const frameworkOptions: DockviewFrameworkOptions = {
-          createLeftHeaderActionComponent: createGroupControlElement(
-            props.leftHeaderActionsComponent,
-            { addPortal }
-          ),
-          createRightHeaderActionComponent: createGroupControlElement(
-            props.rightHeaderActionsComponent,
-            { addPortal }
-          ),
-          createPrefixHeaderActionComponent: createGroupControlElement(
-            props.prefixHeaderActionsComponent,
-            { addPortal }
-          ),
-          createComponent: (options) => {
-            return new SolidPanelContentPart(
-              options.id,
-              props.components[options.name],
-              { addPortal }
-            );
-          },
-          createTabComponent: (options) => {
-            return new SolidPanelHeaderPart(
-              options.id,
-              frameworkTabComponents[options.name],
-              { addPortal }
-            );
-          },
-          createWatermarkComponent: props.watermarkComponent
-            ? () => {
-              return new SolidWatermarkPart(
-                "watermark",
-                props.watermarkComponent!,
-                { addPortal }
-              );
-            }
-            : undefined,
-          defaultTabComponent: props.defaultTabComponent ? DEFAULT_SOLID_TAB : undefined,
-        };
-
-        const api = createDockview(domRef, {
-          ...extractCoreOptions(props),
-          ...frameworkOptions,
-        });
-        const { clientWidth, clientHeight } = domRef;
-        api.layout(clientWidth, clientHeight);
-
-        if (props.onReady) {
-          props.onReady({ api });
-        }
-
-        setDockviewRef(api); // ✅ CORRECTED HERE
-
-        onCleanup(() => {
-          setDockviewRef(undefined);
-          api.dispose();
-        });
+        start();
       });
     });
   });
 
+  // Prop updates
   createEffect(() => {
     const ref = dockviewRef();
     const changes: Partial<DockviewOptions> = {};
-
 
     PROPERTY_KEYS_DOCKVIEW.forEach((propKey) => {
       if ((propKey as keyof DockviewOptions) in props) {
@@ -169,55 +171,47 @@ export function DockviewSolid(props: IDockviewSolidProps) {
       }
     });
 
-    if (ref) {ref.updateOptions(changes);}
+    if (ref && Object.keys(changes).length) {
+      ref.updateOptions(changes);
+    }
     prevProps = { ...props };
   });
 
+  // onDidDrop
   createEffect(() => {
     const ref = dockviewRef();
-    if (!ref) {
-      return;
-    }
-
+    if (!ref) return;
     const disposable = ref.onDidDrop((event) => {
       props.onDidDrop?.(event);
     });
-    onCleanup(() => {
-      disposable.dispose();
-    });
+    onCleanup(() => disposable.dispose());
   });
 
+  // onWillDrop
   createEffect(() => {
     const ref = dockviewRef();
-    if (!ref) {
-      return;
-    }
+    if (!ref) return;
     const disposable = ref.onWillDrop((event) => {
       props.onWillDrop?.(event);
     });
-    onCleanup(() => {
-      disposable.dispose();
-    });
+    onCleanup(() => disposable.dispose());
   });
 
-  const update = (label: string, updater: (ref: DockviewApi) => Partial<DockviewComponentOptions>) => {
+  // Helpers to update dynamic creators
+  const update = (
+    _label: string,
+    updater: (ref: DockviewApi) => Partial<DockviewComponentOptions>
+  ) => {
     createEffect(() => {
       const ref = dockviewRef();
-      if (!ref) {
-        return;
-      }
+      if (!ref) return;
       ref.updateOptions(updater(ref));
     });
   };
 
   update("createComponent", () => ({
-    createComponent: (options) => {
-      return new SolidPanelContentPart(
-        options.id,
-        props.components[options.name],
-        { addPortal }
-      );
-    },
+    createComponent: (options) =>
+      new SolidPanelContentPart(options.id, props.components[options.name], { addPortal }),
   }));
 
   update("createTabComponent", () => {
@@ -227,25 +221,14 @@ export function DockviewSolid(props: IDockviewSolidProps) {
     }
     return {
       defaultTabComponent: props.defaultTabComponent ? DEFAULT_SOLID_TAB : undefined,
-      createTabComponent: (options) => {
-        return new SolidPanelHeaderPart(
-          options.id,
-          frameworkTabComponents[options.name],
-          { addPortal }
-        );
-      },
+      createTabComponent: (options) =>
+        new SolidPanelHeaderPart(options.id, frameworkTabComponents[options.name], { addPortal }),
     };
   });
 
   update("createWatermarkComponent", () => ({
     createWatermarkComponent: props.watermarkComponent
-      ? () => {
-        return new SolidWatermarkPart(
-          "watermark",
-          props.watermarkComponent!,
-          { addPortal }
-        );
-      }
+      ? () => new SolidWatermarkPart("watermark", props.watermarkComponent!, { addPortal })
       : undefined,
   }));
 
@@ -270,17 +253,16 @@ export function DockviewSolid(props: IDockviewSolidProps) {
     ),
   }));
 
-
   return (
     <div
-      ref={(el) => {domRef = el;}}
+      ref={(el) => { domRef = el; }}
       style={{ height: "100%", width: "100%" }}
     >
       {/* {portals()} */}
     </div>
   );
-
 }
+
 
 
 
